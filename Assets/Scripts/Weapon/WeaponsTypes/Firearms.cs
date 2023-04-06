fff879c71e8029fb4515926e09f859c84bbfff88
Maksym Godovanets <maksymgodovanets@gmail.com>
2023-04-06 19:56:24 +0200

    Added method of checking the ability to use weapons.

--- Assets/Scripts/Weapon/WeaponsTypes/Firearms.cs
+++ Assets/Scripts/Weapon/WeaponsTypes/Firearms.cs
@@ -3,27 +3,44 @@ using UnityEngine;
 public class Firearms : Weapon
 {
     [SerializeField] private int bullets;
+    private int currentBullets;
+
+    private void Start()
+    {
+        currentBullets = bullets;
+    }
 
     public override void Use()
     {
-        if (bullets > 0)
+        if (!IsEmpty())
         {
             Fire();
         }
         else
         {
-            ClipIsEmpty();
+            ClipEmpty();
         }
     }
 
     private void Fire()
     {
-        bullets--;
+        currentBullets--;
         Debug.Log($"Pew Pew: {WeaponName}");
     }
 
-    private void ClipIsEmpty()
+    private void ClipEmpty()
     {
         Debug.Log($"Clip is Empty: {WeaponName}");
+    }
+
+    private bool IsEmpty()
+    {
+        if (currentBullets == 0) return true;
+        else return false;
+    }
+
+    private void Reload()
+    {
+        currentBullets = bullets;
     }
 }
--- Assets/Scripts/Weapon/WeaponsTypes/Melee.cs
+++ Assets/Scripts/Weapon/WeaponsTypes/Melee.cs
@@ -3,27 +3,44 @@ using UnityEngine;
 public class Melee : Weapon
 {
     [SerializeField] private int durability;
+    private int currentDurability;
+
+    private void Start()
+    {
+        currentDurability = durability;
+    }
 
     public override void Use()
     {
-        if (durability > 0)
+        if (!IsBroken())
         {
             Hit();
         }
         else
         {
-            WeaponIsBroke();
+            WeaponBroke();
         }
     }
 
     private void Hit()
     {
-        durability--;
+        currentDurability--;
         Debug.Log($"Hit: {WeaponName}");
     }
 
-    private void WeaponIsBroke()
+    private void WeaponBroke()
     {
         Debug.Log($"Weapon is Broke: {WeaponName}");
+    }
+
+    private bool IsBroken()
+    {
+        if (currentDurability == 0) return true;
+        else return false;
+    }
+
+    private void Repair()
+    {
+        currentDurability = durability;
     }
 }
